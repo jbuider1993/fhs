c7c146e3abe0c466cc3bb14c235b7ec52c1a8767
wanglei <wanglei@ylmo2o.com>
2019-04-19 17:56:23 +0800

    给jpa 扫描类添加bean别名

--- fhs_framework_parent/src/main/java/com/fhs/config/MybatisConfig.java
+++ fhs_framework_parent/src/main/java/com/fhs/config/MybatisConfig.java
@@ -1,9 +1,14 @@
 package com.fhs.config;
 
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.fhs.common.utils.ConverterUtils;
+import com.fhs.core.config.EConfig;
 import com.mybatis.jpa.core.PersistentEnhancerScaner;
+import com.mybatis.jpa.xml.XMLMapperLoader;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +25,8 @@ import org.springframework.context.annotation.Configuration;
  * @Version: 1.0
  */
 @Configuration
+@AutoConfigureAfter(MybatisPlusAutoConfiguration.class)
 public class MybatisConfig {
-
     @Value("${fhs.mybatis-jpa.entity-package}")
     private String entityPackage;
 
@@ -31,7 +36,7 @@ public class MybatisConfig {
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
-    @Bean
+    @Bean("persistentEnhancerScaner")
     public PersistentEnhancerScaner getPersistentEnhancerScaner(){
         PersistentEnhancerScaner scanner = new PersistentEnhancerScaner();
         scanner.setEntityPackage(entityPackage);
@@ -40,4 +45,10 @@ public class MybatisConfig {
         return scanner;
     }
 
+    @Bean
+    public XMLMapperLoader getXMLMapperLoader(){
+        XMLMapperLoader loader = new XMLMapperLoader();
+        loader.setEnabled(ConverterUtils.toBoolean(EConfig.getOtherConfigPropertiesValue("isDevModel")));//开启xml热加载
+        return loader;
+    }
 }
