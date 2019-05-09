f3d5c855a19dc99a6a5165641abb237035cd917c
wanglei <wanglei@ylmo2o.com>
2019-05-09 16:46:26 +0800

    更换消息转换器

--- fhs_framework_parent/src/main/java/com/fhs/config/MessageConverterConfig.java
+++ fhs_framework_parent/src/main/java/com/fhs/config/MessageConverterConfig.java
@@ -3,6 +3,7 @@ package com.fhs.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fhs.core.jsonfilter.converter.JsonFilterHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -19,7 +20,7 @@ public class MessageConverterConfig extends  WebMvcConfigurerAdapter
 {
     @Bean
     public HttpMessageConverter<?> fastJsonHttpMessageConverters(){
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        JsonFilterHttpMessageConverter fastJsonHttpMessageConverter = new JsonFilterHttpMessageConverter();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setCharset(Charset.forName("UTF-8"));
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
