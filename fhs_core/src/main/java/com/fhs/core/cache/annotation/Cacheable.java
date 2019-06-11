b573a03a436112716650ff81fb926f309c620c15
wanglei <wanglei@ylmo2o.com>
2019-06-11 18:46:07 +0800

    添加自定义缓存注解

---
+++ fhs_core/src/main/java/com/fhs/core/cache/annotation/Cacheable.java
@@ -0,0 +1,14 @@
+package com.fhs.core.cache.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 代表此类对应的service需要自动缓存
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE})
+public @interface Cacheable {
+}
