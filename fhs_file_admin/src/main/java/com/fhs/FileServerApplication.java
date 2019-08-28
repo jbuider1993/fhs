74a8b9875d98ea7c73f8279758e46cee64a241de
wanglei <wanglei@ylmo2o.com>
2019-08-28 15:36:59 +0800

    解决fhs_file 修改包后的一系列问题

--- fhs_file_admin/src/main/java/com/fhs/FileServerApplication.java
+++ fhs_file_admin/src/main/java/com/fhs/FileServerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+/**
+ * 文件服务
+ * @author wanglei
+ */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.fhs.**.dao"})
 @EnableConfigurationProperties
